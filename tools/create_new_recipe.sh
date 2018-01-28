ce19dab273188a4fbf21b5cf64b1d9b0ecefde3a
lizhieffe <lizhieffe@gmail.com>
2018-01-28 15:00:48 -0800

    add script to create new recipe folder

---
+++ tools/create_new_recipe.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+read -p "Enter package name: " PACKAGE
+read -p "Enter class name: " CLASS_NAME
+
+cd ..
+mkdir "$PACKAGE"
+cd "$PACKAGE"
+cp ../data/script_templates/*.sh .
+sed -i "s:PACKAGE_PLACEHOLDER:${PACKAGE}:g" set_env.sh
+sed -i "s:CLASS_NAME_PLACEHOLDER:${CLASS_NAME}:g" set_env.sh
+. ./create_java_dir.sh
+cd ../tools
